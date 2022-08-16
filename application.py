83a26af956e5485c50627869ed1b3345316caea2
Manpreet Kaur <mk1835466@gmail.com>
2022-08-16 13:09:02 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return 'Hello, World! from manpreet.../.
+    return 'Hello, World! from manpreet.001./.
 if __name__ == "__main__":
     application.debug = True
     application.run(host="0.0.0.0")
